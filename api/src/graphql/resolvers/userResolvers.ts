41de86db33bf12bccea6bc841b0e11f9c9adc276
SarahOtmane <sarahotmane02@gmail.com>
2025-03-20 16:48:11 +0100

    Ajout des résolveurs GraphQL pour le modèle User avec les requêtes et mutations nécessaires

---
+++ api/src/graphql/resolvers/userResolvers.ts
@@ -0,0 +1,34 @@
+import User from '../../models/User';
+import bcrypt from 'bcrypt';
+
+const userResolvers = {
+  Query: {
+    users: async () => {
+      return await User.findAll({ attributes: { exclude: ['password'] } });
+    },
+    user: async (_: any, { id }: { id: string }) => {
+      return await User.findByPk(id, { attributes: { exclude: ['password'] } });
+    }
+  },
+  Mutation: {
+    createUser: async (_: any, { email, firstname, lastname, password, role, id_avatar }: 
+      { email: string; firstname: string; lastname: string; password: string; role?: string; id_avatar?: string }) => {
+
+      // Hash du mot de passe
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = await User.create({
+        email,
+        firstname,
+        lastname,
+        password: hashedPassword,
+        role: role || 'user',
+        id_avatar
+      });
+
+      return { ...user.toJSON(), password: "******" }; // Ne jamais renvoyer le password en clair
+    }
+  }
+};
+
+export default userResolvers;
