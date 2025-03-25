65f4808076b4bde2960470108f1c2679eda135bf
SarahOtmane <sarahotmane02@gmail.com>
2025-03-25 19:31:04 +0100

    Ajout du schéma GraphQL pour la gestion des avatars avec les types et les requêtes associées

---
+++ api/src/graphql/schemas/avatarSchema.ts
@@ -0,0 +1,19 @@
+import { gql } from 'apollo-server-express';
+
+export const avatarTypeDefs = gql`
+  type Avatar {
+    id_avatar: ID!
+    picture_avatar: String!
+  }
+
+  type Query {
+    getAllAvatars: [Avatar]
+  }
+
+  type Mutation {
+    addAvatar(picture_avatar: String!): String
+    updateAvatar(id_avatar: ID!, picture_avatar: String!): String
+    deleteAvatar(id_avatar: ID!): String
+    replaceUserAvatar(id_avatar: ID!): String
+  }
+`;
