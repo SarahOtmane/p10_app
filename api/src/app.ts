c0c4379d1db1a3f478dec7f198a9838e9bf898e4
SarahOtmane <sarahotmane02@gmail.com>
2025-03-28 18:45:08 +0100

    Ajout de la gestion de l'authentification JWT dans le contexte Apollo et mise à jour des chemins de types

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,6 +4,8 @@ import { connectDB } from './config/database';
 import { typeDefs, resolvers } from './graphql';
 import TableManager from './config/tableManager';
 import path from 'path';
+import jwt from 'jsonwebtoken';
+import { MyContext } from './types/context';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,8 +20,31 @@ app.use('/images', express.static(path.join(__dirname, '../public/images')));
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req, res }) => ({ req, res }),
+  context: async ({ req, res }: { req: Request; res: Response }): Promise<MyContext> => {
+    const authHeader = req.headers['authorization'];
+
+    if (authHeader) {
+      try {
+        const payload = await new Promise((resolve, reject) => {
+          jwt.verify(authHeader, process.env.JWT_KEY as string, (err, decoded) => {
+            if (err) reject(err);
+            else resolve(decoded);
+          });
+        });
+
+        (req as any).user = payload;
+      } catch (err) {
+        console.warn("Token invalide :", err);
+      }
+    }
+
+    return {
+      req: req as unknown as MyContext['req'],
+      res: res as unknown as MyContext['res']
+    };
+  }
 });
+
 
 async function startServer() {
   await server.start();
