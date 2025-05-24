00c785a7afe18393242f3fbef6bee44d51c65250
SarahOtmane <sarahotmane02@gmail.com>
2025-05-24 19:28:52 +0200

    Ajout du schéma GraphQL pour la gestion des écuries : définition des types, entrées et mutations.

---
+++ api/src/graphql/schemas/ecurieSchema.ts
@@ -0,0 +1,29 @@
+import { gql } from 'apollo-server-express';
+
+const ecurieTypeDefs = gql`
+  type Ecurie {
+    id_api_ecurie: Int!
+    name: String!
+    short_name: String
+    logo: String
+    color: String
+  }
+
+  input UpdateEcurieInput {
+    id_api_ecurie: Int!
+    name: String
+    short_name: String
+    logo: String
+    color: String
+  }
+
+  type Query {
+    getEcuries: [Ecurie!]!
+  }
+
+  type Mutation {
+    updateEcurie(input: UpdateEcurieInput!): Ecurie
+  }
+`;
+
+export default ecurieTypeDefs;
